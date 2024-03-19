3f8235dc9bf8c2013e539dcec499e838880f21af
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-03-19 23:56:47 +0530

    week 8.0 - videoCard component and tailwind setup

---
+++ cohort/week 8/you-tube-frontend/src/components/VideoCard.tsx
@@ -0,0 +1,21 @@
+export function VideoCard(props:any) {
+  return (
+    <div>
+      <img className="rounded-xl" src={props.image} alt="you-tube-thumbnail" />
+      <div className="grid grid-cols-12 pt-2">
+        <div className="col-span-1">
+          <img
+            className="rounded-full w-12 h-12"
+            src={props.channelImage}
+            alt="you-tube-thumbnail"
+          />
+        </div>
+        <div className="col-span-11 pl-2">
+          <h3>{props.title}</h3>
+          <p className="text-gray-400 text-base">{props.channel}</p>
+          <p className="text-gray-400 text-base">{props.views} | {props.timestamp}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
