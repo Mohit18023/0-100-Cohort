dc9fcda22d102441a8956381002a8222be45f0ca
mohit choudhary <mohitchoudhary1054@gmail.com>
2024-03-27 23:53:24 +0530

    hono-app added

--- cohort/week 11/11.1/my-app/src/index.ts
+++ cohort/week 11/11.1/my-app/src/index.ts
@@ -27,20 +27,8 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		console.log(request.body);
-		console.log(request.headers);
-		console.log(request.method);
-		var uri = request.url.replace(/^https:\/\/.*?\//gi,"/");
-		console.log(uri)
-
-		if (request.method === 'GET') {
-			return Response.json({
-				message: 'you sent a get request',
-			});
-		} else {
-			return Response.json({
-				message: 'you did not send a get request',
-			});
-		}
+		return Response.json({
+			message:"Wassup"
+		})
 	},
 };
